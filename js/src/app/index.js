0e04586f1191aeda390a56f7385651c26d965311
hjx422 <hjx422@gmail.com>
2018-05-24 23:14:13 +0800

    update: 重置浏览器默认样式

--- js/src/app/index.js
+++ js/src/app/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import 'normalize.css'
 
 class Index extends React.Component {
   //static propTypes = {
